014730baafa1668b0764a56f9edf6737e3136eb1
Takano1004 <c4p31061@bunkyo.ac.jp>
2025-05-29 20:31:42 +0900

    Update route.py

--- 7回目プログラム/route.py
+++ 7回目プログラム/route.py
@@ -1,19 +1,20 @@
-from flask import request, redirect, send_from_directory
+from flask import request, render_template
 import controller
 
-def create_route(app):
+def create_route(app):  
 
     @app.route("/")
     def index():
         return controller.top()
-    
-    @app.route("/json", methods=["GET","POST"])
+
+    @app.route("/json", methods=["GET", "POST"])
     def json():
         if "data" in request.args:
             d = request.args.get("data")
-            if d in ["user","item","category","dept"]:
+            if d in ["user", "item", "category", "dept"]:
                 return controller.json(d)
-        return("GETパラメータを指定してください")
-     @app.route("/lost-user")
+        return "GETパラメータを指定してください"
+
+    @app.route("/lost-user")
     def lost_user():
         return controller.lost_user()
