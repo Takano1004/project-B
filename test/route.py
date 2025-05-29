ea23939e3f75c6c3b4c18888c1161bf17339f2e7
Takano1004 <c4p31061@bunkyo.ac.jp>
2025-05-29 23:30:23 +0900

    Add files via upload

---
+++ test/route.py
@@ -0,0 +1,18 @@
+from flask import request, render_template
+import controller
+
+def create_route(app):  
+
+    @app.route("/")
+    def index():
+        return controller.top()
+
+    @app.route("/json", methods=["GET", "POST"])
+    def json():
+        if "data" in request.args:
+            d = request.args.get("data")
+            if d in ["user", "item", "category", "dept"]:
+                return controller.json(d)
+        return "GETパラメータを指定してください"
+    @app.route("/lost-user")
+    def lost_user():
